0253806214f0011b6af02a176529c73d5cbbb4c2
rokas <rokasra@gmail.com>
2022-05-23 18:30:16 +0300

    nav bar

--- src/config.js
+++ src/config.js
@@ -3,12 +3,13 @@ require('dotenv').config();
 const PORT = process.env.PORT || 5000;
 
 const dbConfig = {
-  host: process.env.HOST,
-  user: process.env.USER,
-  password: process.env.PASSWORD,
-  port: process.env.PORT,
-  database: process.env.DB,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+  database: process.env.DB_DB,
 };
+
 console.log('dbConfig===', dbConfig);
 
 module.exports = {
--- src/server.js
+++ src/server.js
@@ -1,21 +1,39 @@
+/* eslint-disable func-names */
+/* eslint-disable prefer-arrow-callback */
 const express = require('express');
-const morgan = require('morgan');
+const path = require('path');
 const { PORT } = require('./config');
 
 const app = express();
 
-// Middleware
-app.use(morgan('dev'));
-app.use(express.json());
+app.set('view engine', 'ejs');
+app.set('views', 'src/views');
 
-app.get('/', (req, res) => {
-  res.send({ msg: 'Server is running' });
+const staticDir = path.join(__dirname, 'assets');
+app.use(express.static(staticDir));
+
+app.get('/', function (req, res) {
+  // get info from db
+  const tech = ['Html', 'Css', 'Js'];
+  const data = {
+    tech,
+    title: 'Home page',
+  };
+  res.render('index', data);
 });
 
-// Routes
+// // Middleware
+// app.use(morgan('dev'));
+// app.use(express.json());
 
-app.all('*', (req, res) => {
-  res.status(400).send({ error: 'Page not found' });
-});
+// app.get('/', (req, res) => {
+//   res.send({ msg: 'Server is running' });
+// });
+
+// // Routes
+
+// app.all('*', (req, res) => {
+//   res.status(400).send({ error: 'Page not found' });
+// });
 
 app.listen(PORT, () => console.log('Listening on port', PORT));
